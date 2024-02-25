f94f7617ab21533f1954b2eb3c0af4d0af4aa209
Zainab Ali <47698736+DznEggs4Keira@users.noreply.github.com>
2024-02-25 17:03:42 +0100

    Junaid commit please check

--- Assets/NPC/Npc.cs
+++ Assets/NPC/Npc.cs
@@ -12,6 +12,13 @@ public class npc : MonoBehaviour
         // Get the NavMeshAgent component attached to the cube.
         agent = GetComponent<NavMeshAgent>();
 
+        // set random speed of the npc car
+        agent.speed = Random.Range(3.5f, 10);
+        Debug.Log(agent.name + " " + agent.speed);
+
+        // set random handling of the npc car
+
+
         // Set the cube's initial position on the road start.
         agent.Warp(roadStart.position);
 
