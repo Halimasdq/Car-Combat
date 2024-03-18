51c81d6d323daaff17465b2d6dfb87d91aa09bfa
HalimaSDQ <haimasiddique12@gmail.com>
2024-03-18 23:45:29 +0500

    update

--- Assets/Menus/Loading scenes.cs
+++ Assets/Menus/Loading scenes.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -40,3 +41,61 @@ public class Loadingscenes : MonoBehaviour
     }
 
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loadingscenes : MonoBehaviour
+{
+    public GameObject Loading;
+    public Slider loader;
+
+    public GameObject[] characters; // 0 , 1
+    public Transform startingPosition;
+
+   /* void Awake()
+    {
+        // Check if PlayerPrefs has the selected main character
+        int mainCharacter = PlayerPrefs.GetInt("SelectedCarID");
+        // 1 - black car
+        // 2 - white car
+
+        // Find the selected main character in the array and spawn it
+        Instantiate(characters[mainCharacter - 1], startingPosition.position, startingPosition.rotation);
+    }*/
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine (LoadAsynchronously  (sceneIndex));
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        Loading.SetActive (true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            loader.value = progress;
+            yield return null;
+        }
+    }
+
+    public void QuitApplicationFun()
+    {
+        Application.Quit();
+    }
+
+}
+>>>>>>> Stashed changes
--- Assets/Player/carcontroller.cs
+++ Assets/Player/carcontroller.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -65,3 +66,111 @@ public class CarController : MonoBehaviour
         bombRigidbody.AddForce(throwPoint.forward * throwForce, ForceMode.Impulse);
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelCollider RRCollider;
+    public WheelCollider RLCollider;
+    public WheelCollider FRCollider;
+    public WheelCollider FLCollider;
+
+    public float maxTorque = 100f;
+    public float maxBrakeTorque = 3000f;
+    public float maxSteerAngle = 30f;
+    public float steerLerpSpeed = 5f; // Adjust this value to control steering smoothness
+
+    public GameObject bombPrefab;
+    public Transform throwPoint;
+    public float throwForce = 10f;
+
+    private Rigidbody rb;
+
+    private float carAccel;
+    private float carSteer;
+
+    private float currentSteerAngle = 0f;
+
+    public float speed;
+
+    private HealthBar healthBar;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // subscribe to the event
+        //HealthBar.OnDamageTaken += ;
+    }
+
+    private void OnDestroy()
+    {
+        //unsubscribe to the event
+        //HealthBar.OnDamageTaken -= ;
+    }
+
+    void FixedUpdate()
+    {
+        carAccel = Input.GetAxis("Vertical");
+        carSteer = Input.GetAxis("Horizontal");
+
+        float handbrake = Input.GetKey(KeyCode.Space) ? maxBrakeTorque : 0f;
+
+        ApplyTorque(carAccel, handbrake);
+        ApplySteer(carSteer);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ThrowBomb();
+        }
+    }
+
+    void ApplyTorque(float accelInput, float brakeTorque)
+    {
+        float torque = accelInput * maxTorque;
+
+        // use for speedometer
+        speed = torque;
+
+        RLCollider.brakeTorque = brakeTorque;
+        RRCollider.brakeTorque = brakeTorque;
+        FLCollider.brakeTorque = brakeTorque;
+        FRCollider.brakeTorque = brakeTorque;
+
+        RLCollider.motorTorque = torque;
+        RRCollider.motorTorque = torque;
+    }
+
+    void ApplySteer(float steerInput)
+    {
+        float turnFactor = 2f; // Adjust this value to control turning sensitivity
+        float targetSteerAngle = steerInput * maxSteerAngle * turnFactor;
+        currentSteerAngle = Mathf.Lerp(currentSteerAngle, targetSteerAngle, Time.deltaTime * steerLerpSpeed);
+
+        FLCollider.steerAngle = currentSteerAngle;
+        FRCollider.steerAngle = currentSteerAngle;
+    }
+
+    void ThrowBomb()
+    {
+        GameObject bomb = Instantiate(bombPrefab, throwPoint.position, throwPoint.rotation);
+        Rigidbody bombRigidbody = bomb.GetComponent<Rigidbody>();
+        bombRigidbody.AddForce(throwPoint.forward * throwForce, ForceMode.Impulse);
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Bullet"))
+        {
+            // take damage
+            // event call to take damage
+            //HealthBar.DamageTaken(10);
+        }
+    }*/
+}
+
+
+>>>>>>> Stashed changes
