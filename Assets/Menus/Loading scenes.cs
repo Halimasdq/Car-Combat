8dde0b4195df43ef411b7cdffc895e8ed687f18b
junaidkhanniazi <107788602+junaidkhanniazi@users.noreply.github.com>
2024-03-23 08:41:29 -0700

    Revert "Merge branch 'last-updates' into Junaid"
    This reverts commit fe6ff17895b6691334a25f030c3b9980b0bc963e, reversing
    changes made to 317abfd2aad3413485a4b28b0b88324eccafdb68.

--- Assets/Menus/Loading scenes.cs
+++ Assets/Menus/Loading scenes.cs
@@ -1,47 +1,3 @@
-<<<<<<< Updated upstream
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-
-public class Loadingscenes : MonoBehaviour
-{
-    public GameObject Loading;
-    public Slider loader;
-    
-    public void LoadLevel (int sceneIndex)
-    {
-        StartCoroutine (LoadAsynchronously  (sceneIndex));
-        if (Time.timeScale == 0f)
-        {
-            Time.timeScale = 1.0f;
-        }
-        else
-        {
-            Time.timeScale = 1f;
-        }
-    }
-
-    IEnumerator LoadAsynchronously (int sceneIndex)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-        Loading.SetActive (true);
-        while (!operation.isDone)
-        {
-            float progress = Mathf.Clamp01(operation.progress / .9f);
-            loader.value = progress;
-            yield return null;
-        }
-    }
-
-    public void QuitApplicationFun()
-    {
-        Application.Quit();
-    }
-
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -56,7 +12,7 @@ public class Loadingscenes : MonoBehaviour
     public GameObject[] characters; // 0 , 1
     public Transform startingPosition;
 
-   /* void Awake()
+    void Awake()
     {
         // Check if PlayerPrefs has the selected main character
         int mainCharacter = PlayerPrefs.GetInt("SelectedCarID");
@@ -65,7 +21,7 @@ public class Loadingscenes : MonoBehaviour
 
         // Find the selected main character in the array and spawn it
         Instantiate(characters[mainCharacter - 1], startingPosition.position, startingPosition.rotation);
-    }*/
+    }
 
     public void LoadLevel (int sceneIndex)
     {
@@ -98,4 +54,3 @@ public class Loadingscenes : MonoBehaviour
     }
 
 }
->>>>>>> Stashed changes
--- Assets/Player/carcontroller.cs
+++ Assets/Player/carcontroller.cs
@@ -1,72 +1,3 @@
-<<<<<<< Updated upstream
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CarController : MonoBehaviour
-{
-    public WheelCollider RRCollider;
-    public WheelCollider RLCollider;
-    public WheelCollider FRCollider;
-    public WheelCollider FLCollider;
-
-    public float maxTorque = 100f;
-    public float maxBrakeTorque = 3000f;
-    public float maxSteerAngle = 30f;
-
-    public GameObject bombPrefab;
-    public Transform throwPoint;
-    public float throwForce = 10f;
-
-    private float carAccel;
-    private float carSteer;
-
-    void FixedUpdate()
-    {
-        carAccel = Input.GetAxis("Vertical");
-        carSteer = Input.GetAxis("Horizontal");
-
-        if (Input.GetKey(KeyCode.Space)) // handbrake
-        {
-            float handbrakeTorque = maxBrakeTorque;
-
-            RLCollider.brakeTorque = handbrakeTorque;
-            RRCollider.brakeTorque = handbrakeTorque;
-            FLCollider.brakeTorque = handbrakeTorque;
-            FRCollider.brakeTorque = handbrakeTorque;
-
-            RLCollider.motorTorque = 0f;
-            RRCollider.motorTorque = 0f;
-        }
-        else
-        {
-            RLCollider.brakeTorque = 0f;
-            RRCollider.brakeTorque = 0f;
-            FLCollider.brakeTorque = 0f;
-            FRCollider.brakeTorque = 0f;
-
-            // Adjusted for sharper and smoother turning
-            float turnFactor = 2f; // Adjust this value to control turning sensitivity
-            FLCollider.steerAngle = carSteer * maxSteerAngle * turnFactor;
-            FRCollider.steerAngle = carSteer * maxSteerAngle * turnFactor;
-
-            RLCollider.motorTorque = carAccel * maxTorque;
-            RRCollider.motorTorque = carAccel * maxTorque;
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            ThrowBomb();
-        }
-    }
-    void ThrowBomb()
-    {
-        GameObject bomb = Instantiate(bombPrefab, throwPoint.position, throwPoint.rotation);
-        Rigidbody bombRigidbody = bomb.GetComponent<Rigidbody>();
-        bombRigidbody.AddForce(throwPoint.forward * throwForce, ForceMode.Impulse);
-    }
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -161,7 +92,7 @@ public class CarController : MonoBehaviour
         bombRigidbody.AddForce(throwPoint.forward * throwForce, ForceMode.Impulse);
     }
 
-    /*private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Bullet"))
         {
@@ -169,8 +100,7 @@ public class CarController : MonoBehaviour
             // event call to take damage
             //HealthBar.DamageTaken(10);
         }
-    }*/
+    }
 }
 
 
->>>>>>> Stashed changes
